bc7d2c8325c174ed8bba8deca5094051d4112a77
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-04-06 21:16:31 +0600

    Start Tshirt Store

---
+++ src/components/OrdersReview/OrdersReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersReview = () => {
+    return (
+        <div>
+            <h2>Order Review page</h2>
+        </div>
+    );
+};
+
+export default OrdersReview;
